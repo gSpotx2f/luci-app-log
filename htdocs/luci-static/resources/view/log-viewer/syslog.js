7de10f55fee9a6f34b418d685448125bd0924c2b
gSpot <gspot.x2f@gmail.com>
2025-04-14 03:21:35 +0300

    syslog.js: fixed auto update.

--- htdocs/luci-static/resources/view/log-viewer/syslog.js
+++ htdocs/luci-static/resources/view/log-viewer/syslog.js
@@ -2,9 +2,9 @@
 'require view.log-viewer.log-system as abc';
 
 return abc.view.extend({
-	viewName   : 'syslog',
-	title      : _('System Log'),
-	autoRefresh: true,
-	appPattern : '^',
-	loggerTail : true,
+	viewName         : 'syslog',
+	title            : _('System Log'),
+	enableAutoRefresh: true,
+	appPattern       : '^',
+	loggerTail       : true,
 });
